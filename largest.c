fbec33fd12aa49335ae06bdba9184a0f04f51907
Md. Tahmid Shah Tamim <119511850+tamimshah@users.noreply.github.com>
2023-03-03 20:59:02 +0600

    Update largest.c

--- largest.c
+++ largest.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 main()
 //MD.TAHMID SHAH TAMIM
-//ID.222-35-1141
+ 
 {
 int a,b,c,result;
 printf("Enter Three Numbers:\n");
 scanf("%d%d%d",&a,&b,&c);
 result=(a>b)&&(a>c)? a:(b>c)?b:c;
 printf("The Largest Number is: %d",result);
-/*jnwegewffgnfd */
+ 
 return 0;
 }
